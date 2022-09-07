c9622fe8735d035c0b71481189d9db1650639252
jplebreton5 <21118434@student.uwa.edu.au>
2022-09-07 09:57:44 +0800

    class class template

---
+++ GameObject.java
@@ -0,0 +1,9 @@
+public class GameObject {
+    private static int count = 0;
+
+    public int id;
+
+    public GameObject() {
+        id = count++;
+    }
+}
--- Green.java
+++ Green.java
@@ -1,2 +1,33 @@
-public class Green {
+import java.util.Vector;
+
+public class Green extends GameObject {
+    double uncertainty;
+    double du;
+    boolean willVote;
+    boolean followsRed;
+    Vector<Green> friends;
+
+    public Green(int id, boolean willVote, boolean followsRed) {
+        super();
+        this.willVote = willVote;
+        this.followsRed = followsRed;
+        friends = new Vector<Green>();
+    }
+
+    public void calcNewUncertainty() {
+        double sum = 0;
+        for (Green g : friends) {
+            sum += g.uncertainty;
+        }
+        double avg = sum / friends.size();
+        du = avg - uncertainty;
+    }
+
+    public void update() {
+        uncertainty += du;
+        du = 0;
+        if (Math.random() < (uncertainty + 1) / 2) {
+            willVote = !willVote;
+        }
+    }
 }

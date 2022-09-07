74729a8ac9459ef3802bfc46e324cd9a783199c3
Iash5225 <iash.bashir5225@gmail.com>
2022-09-07 10:50:03 +0800

    Working on potent messaging

--- Game.java
+++ Game.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Game {
     public Vector<Green> greens;
+    public Red rednode;
 
     public Game(int n_green, double prob_edge, int n_grey, double prob_spy, double uncertainty_lb, double uncertainty_ub, double percentage_vote) {
         greens = new Vector<Green>();
@@ -12,10 +13,31 @@ public class Game {
         }
         for (Green g : greens) {
             for (Green g2 : greens) {
-                if (g != g2 && Math.random() < prob_edge) {
+                if ( g != g2 && Math.random() < prob_edge ){
                     g.friends.add(g2);
                 }
             }
+        }
+    }
+
+    public void InfluenceGreen(Red rednode) {
+        switch (rednode.levelofMessage) {
+            case 1:
+
+
+                //Increase green uncertainty by an increment of 0.1
+                //check if boudns are still valid]
+                //check if green will vote is false
+
+            case 2:
+//Decrease green uncertainty by an increment of 0.2
+            case 3:
+//Decrease green uncertainty by an increment of 0.3
+            case 4:
+//Decrease green uncertainty by an increment of 0.4
+            case 5:
+//Decrease green uncertainty by an increment of 0.5
+
         }
     }
 

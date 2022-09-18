a716d9d905a78020aca915bc4776e4a02cab4d40
Iash5225 <iash.bashir5225@gmail.com>
2022-09-18 14:17:24 +0800

    InfluenceGreen based on rednode level of messaging is under progress

--- Game.java
+++ Game.java
@@ -20,25 +20,55 @@ public class Game {
         }
     }
 
+    /**
+     *
+     * @param rednode the red node
+     *                1: send level 1 message - 10% of the greens will vote
+     *                2: send level 2 message - 20% of the greens will vote
+     *                3: send level 3 message - 30% of the greens will vote
+     *                4: send level 4 message - 40% of the greens will vote
+     *                5: send level 5 message - 50% of the greens will vote
+     */
     public void InfluenceGreen(Red rednode) {
         switch (rednode.levelofMessage) {
             case 1:
-
+                for (Green g : greens) {
+                    g.uncertainty = g.uncertainty + 0.1;
+                }
 
                 //Increase green uncertainty by an increment of 0.1
-                //check if boudns are still valid]
+                //check if bounds are still valid
                 //check if green will vote is false
 
             case 2:
 //Decrease green uncertainty by an increment of 0.2
+                for (Green g : greens) {
+                    if ( g.uncertainty + 0.2 < 1 ){
+                        g.uncertainty = g.uncertainty + 0.2;
+                    }
+                }
             case 3:
 //Decrease green uncertainty by an increment of 0.3
+                for (Green g : greens) {
+                    if ( g.uncertainty + 0.2 < 1 ){
+                        g.uncertainty = g.uncertainty + 0.3;
+                    }
+                }
             case 4:
 //Decrease green uncertainty by an increment of 0.4
+                for (Green g : greens) {
+                    if ( g.uncertainty + 0.2 < 1 ){
+                        g.uncertainty = g.uncertainty + 0.4;
+                    }
+                }
             case 5:
 //Decrease green uncertainty by an increment of 0.5
+                for (Green g : greens) {
+                    if ( g.uncertainty + 0.2 < 1 ){
+                        g.uncertainty = g.uncertainty + 0.5;
+                    }
+                }
 
         }
     }
-
 }

5aa07bc57e2ad11a5133d3ba122db00d0597a4d3
Iash5225 <iash.bashir5225@gmail.com>
2022-09-23 14:22:46 +0800

    Printing messages

--- Red.java
+++ Red.java
@@ -1,28 +1,32 @@
 public class Red extends GameObject {
     public int levelofMessage;
 
-    public Red (){
+    public Red() {
         super();
-        levelofMessage=0;
+        levelofMessage = 0;
     }
 
     public void setLevelofMessage(int levelofMessage) throws Exception {
 
         this.levelofMessage = levelofMessage;
-        switch(levelofMessage){
+        switch (levelofMessage) {
             case 1:
-
+                System.out.println("Red is sending level 1 message");
                 break;
             case 2:
+                System.out.println("Red is sending level 2 message");
                 break;
             case 3:
+                System.out.println("Red is sending level 3 message");
                 break;
             case 4:
+                System.out.println("Red is sending level 4 message");
                 break;
             case 5:
+                System.out.println("Red is sending level 5 message");
                 break;
             default:
-                throw new Exception("level of messaging is outside the bounds of 0->5");
+                throw new Exception("Invalid level of message");
         }
     }
 }

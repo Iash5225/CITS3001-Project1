267e69bc64c976e7efce587b5c0ffdbaf4d92e0e
Iash5225 <iash.bashir5225@gmail.com>
2022-09-28 20:22:08 +0800

    Working on Game + Blue + Red + added extra IDE code

--- src/Blue.java
+++ src/Blue.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Blue extends GameObject {
 
     public double energy;
--- src/Red.java
+++ src/Red.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Red extends GameObject {
     public int levelofMessage;
 
@@ -7,26 +9,8 @@ public class Red extends GameObject {
     }
 
     public void setLevelofMessage(int levelofMessage) throws Exception {
+        this.levelofMessage = levelofMessage; //work on that
+        System.out.println("Red is sending a level " + levelofMessage + "message");
 
-        this.levelofMessage = levelofMessage;
-        switch (levelofMessage) {
-            case 1:
-                System.out.println("Red is sending level 1 message");
-                break;
-            case 2:
-                System.out.println("Red is sending level 2 message");
-                break;
-            case 3:
-                System.out.println("Red is sending level 3 message");
-                break;
-            case 4:
-                System.out.println("Red is sending level 4 message");
-                break;
-            case 5:
-                System.out.println("Red is sending level 5 message");
-                break;
-            default:
-                throw new Exception("Invalid level of message");
-        }
     }
 }

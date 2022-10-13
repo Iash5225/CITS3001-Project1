a1149bed2428be9420f4e99c493672c60cb64fe4
j-plebreton <21118434@student.uwa.edu.au>
2022-10-13 22:02:39 +0800

    added a Config file for all our constants

---
+++ src/Config.java
@@ -0,0 +1,17 @@
+
+public class Config {
+
+    // Game parameters
+    public static final int N_GREENS = 10;
+    public static final double PROB_EDGE = 0.5;
+    public static final int N_GREY = 3;
+    public static final int N_SPIES = 1;
+    public static final int UNCERTAINTY_LOWER_BOUND = -1;
+    public static final int UNCERTAINTY_UPPER_BOUND = 1;
+    public static final double PROB_VOTE = 0.5;
+
+    // Extra game parameters
+    public static final int N_ROUNDS = 10;
+    public static final int N_GAMES = 1000;
+    public static final double BLUE_STARTING_ENERGY = 10.0;
+}

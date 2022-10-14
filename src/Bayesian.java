66b4a10199eb9f80e4e65b12cdcdf95355d920fd
Iash5225 <iash.bashir5225@gmail.com>
2022-10-14 11:13:37 +0800

    following tutorial

---
+++ src/Bayesian.java
@@ -0,0 +1,12 @@
+public class Bayesian extends GameObject {
+    // TODO Auto-generated constructor stub
+
+    public Game game;
+    public double chance_of_winning;
+
+    public Bayesian(Game game) {
+        super();
+        this.game = game;
+    }
+
+}

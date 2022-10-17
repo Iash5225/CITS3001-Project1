eba5d00ea6a3130864b494354e8befe7c282fb60
j-plebreton <21118434@student.uwa.edu.au>
2022-10-17 23:22:49 +0800

    added uncertainty simulation

--- src/program2.java
+++ src/program2.java
@@ -2,7 +2,7 @@
 import com.gembox.spreadsheet.*;
 import com.gembox.spreadsheet.charts.*;
 
-public class program {
+public class program2 {
 
     public static void main(String[] args) throws java.io.IOException {
         // If using Professional version, put your serial key below.
@@ -12,20 +12,23 @@ public class program {
         ExcelFile ef = new ExcelFile();
         ExcelWorksheet ws = ef.addWorksheet("Chart");
 
-        String[] rounds = new String[] { "50", "100", "150", "200", "250", "300", "350", "400", "450", "500" };
+        double[] u_lbs = { -0.1, -0.5, -1 };
+        double[] u_ubs = { 0.1, 0.5, 1 };
 
-        int Number_of_Round_intervals = rounds.length;
+        String[] u_ranges = { "(-0.1, 0.1)", "(-0.5, 0.5)", "(-1, 1)" };
+
+        int Number_of_Uncertainty_intervals = u_ranges.length;
 
         // Create Excel chart and select data for it.
         ExcelChart chart = ws.getCharts().add(ChartType.BAR, "D2", "M25");
-        chart.selectData(ws.getCells().getSubrangeAbsolute(0, 0, Number_of_Round_intervals, 2), true);
+        chart.selectData(ws.getCells().getSubrangeAbsolute(0, 0, Number_of_Uncertainty_intervals, 2), true);
 
         // Add data which is used by the Excel chart.
-        for (int i = 0; i < Number_of_Round_intervals; i++) {
+        for (int i = 0; i < Number_of_Uncertainty_intervals; i++) {
             ws.getCell(i + 1, 0)
-                    .setValue(rounds[i % rounds.length] + (i < rounds.length ? "" : " " + (i / rounds.length + 1)));
-            int round = Integer.parseInt(rounds[i]);
-            int[] win_summary = sim.round_simulation(round);
+                    .setValue(u_ranges[i % u_ranges.length]
+                            + (i < u_ranges.length ? "" : " " + (i / u_ranges.length + 1)));
+            int[] win_summary = sim.uncertainty_simulation(u_lbs[i], u_ubs[i]);
             int blue_wins = win_summary[0];
             int red_wins = win_summary[1];
             ws.getCell(i + 1, 1).setValue(blue_wins);
@@ -33,14 +36,15 @@ public class program {
         }
 
         // Set header row and formatting.
-        ws.getCell(0, 0).setValue("Round");
+        ws.getCell(0, 0).setValue("Starting Uncertainty range");
         ws.getCell(0, 1).setValue("Blue Wins");
         ws.getCell(0, 2).setValue("Red Wins");
         ws.getCell(0, 1).getStyle().getFont().setWeight(ExcelFont.BOLD_WEIGHT);
         ws.getCell(0, 0).getStyle().getFont().setWeight(ExcelFont.BOLD_WEIGHT);
         ws.getCell(0, 2).getStyle().getFont().setWeight(ExcelFont.BOLD_WEIGHT);
-        ws.getColumn(0).setWidth((int) LengthUnitConverter.convert(3, LengthUnit.CENTIMETER,
+        ws.getColumn(0).setWidth((int) LengthUnitConverter.convert(4, LengthUnit.CENTIMETER,
                 LengthUnit.ZERO_CHARACTER_WIDTH_256_TH_PART));
         ef.save("Chart.xlsx");
     }
+
 }
